dec8028a577c9e31eeaca26207f2cf0d5d4ee867
Jacob Gleason <jacob-gleason@uiowa.edu>
2020-10-30 15:55:45 -0500

    Created seed file for the cards entity. Only seeding a few cards to start with. Still need to create the Card model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+cards = [{:deck_number => '1', :value => 'A', :suit => 'spades', :owned_by => 'none'},
+         {:deck_number => '1', :value => '7', :suit => 'hearts', :owned_by => 'none'},
+         {:deck_number => '1', :value => '9', :suit => 'clubs', :owned_by => 'none'}
+          # add more cards to seed with a full deck of 52 if this works
+        ]
+
+cards.each do |curr_card|
+  Card.create!(curr_card)
+end
