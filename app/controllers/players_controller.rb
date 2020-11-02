63767b6b9794e628f984d4740054310bb7354ed5
mbrzus <michal-brzus@uiowa.edu>
2020-11-02 17:39:46 -0600

    add player controller and schema

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,14 @@
 class PlayersController < ApplicationController
 
+  def player_params
+    # function to extract a player_name string from params object
+    params.require(:players)
+  end
+
+  def create
+    player_params.each do |player|
+      Player.create!(player)
+    end
+  end
+
 end
