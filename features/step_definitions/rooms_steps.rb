920fb607835d3951ca59676dc85857493651d805
Daniel Illg <dillg@uiowa.edu>
2020-11-03 17:31:34 -0600

    updated rooms_steps.rb to reflect the current create new room feature

--- features/step_definitions/rooms_steps.rb
+++ features/step_definitions/rooms_steps.rb
@@ -1,3 +1,14 @@
 Given /^I am on the main page$/ do
   visit rooms_path
 end
+
+When /I have pressed the (create-new-room-button)/ do |button_id|
+  # store the current number of rooms in the database
+  @number_of_rooms = Room.count
+  click_button button_id
+end
+
+Then /a room should be created in the database/ do
+  # the current number of rooms in the database is greater than before
+  expect(Room.count).to be > @number_of_rooms
+end
