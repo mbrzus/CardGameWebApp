8a28a8702cd249556efdd60db3eb9ef1ab802096
Daniel Illg <dillg@uiowa.edu>
2020-11-03 17:51:33 -0600

    added in the spec file for the rooms controller that includes the monkeypatch that fixes rspec tests on our current environment not running

---
+++ spec/controllers/rooms_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'rails_helper'
+
+# monkey patch method found by class member on teams
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
+
