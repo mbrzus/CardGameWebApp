72c0218c580ccfea87c14ed2380ceb1416f68ae5
Daniel Illg <dillg@uiowa.edu>
2020-11-03 18:56:49 -0600

    completed implementation of the create path in the rooms_controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,7 +13,11 @@ class RoomsController < ApplicationController
   end
 
   def create
-
+    # room has no info so just create an empty object
+    new_room = Room.new
+    new_room.save!
+    @room_id = new_room.id
+    redirect_to room_path(:id => @room_id)
   end
 
   def edit
