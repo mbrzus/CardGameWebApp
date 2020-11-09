4f2c0776eefd325c1c114dab19477e3bbd111b00
Daniel Illg <dillg@uiowa.edu>
2020-11-09 12:22:32 -0600

    Now get the proper hash values for the player name and room reference

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,7 +4,7 @@ class Player < ActiveRecord::Base
   # search for the player. If none found, create a new player
   def self.create_or_load(player)
     # look for an existing player with the given name in the given room
-    existing_player = Player.where(name: player["name"]).where(room: player["room"]).first
+    existing_player = Player.where(name: player[:name]).where(room: player[:room]).first
     # if the player doesn't exist, create the player and return the details
     if !existing_player
       return Player.create!(player)
