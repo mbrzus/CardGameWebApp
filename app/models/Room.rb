852ef607c2390c1a119217358dce10e655e76997
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-09 09:19:28 -0600

    edited the room model to create room_token

--- app/models/Room.rb
+++ app/models/Room.rb
@@ -1,3 +1,12 @@
 class Room < ActiveRecord::Base
+  before_save :create_room_token
+
+  def self.create_room!
+    Room.create!
+  end
+
+  def create_room_token
+    self.room_token = SecureRandom.alphanumeric(5)
+  end
 
 end
