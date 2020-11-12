595ce8806871d7c60f645bd8564d8fd962ae85aa
mbrzus <michal-brzus@uiowa.edu>
2020-11-12 11:23:32 -0600

    update card code to the latest version

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,15 +4,11 @@ class Card < ActiveRecord::Base
   @suit
   @image_url
   belongs_to :player
-
   # Helper function that will is called by methods in CardController that are used for card transactions
   def change_owner(new_owner_id)
-    #debugger
-    self.player = new_owner_id
+    self.player_id = new_owner_id
     # Save the changes to the card to the database without validation
     # Resource: https://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-save
     self.save!
   end
-
-
 end
