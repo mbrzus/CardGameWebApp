04e0faca136456560e0a110589186698d0487817
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-13 09:33:25 -0600

    Implemented the controller actions that allow users with an account to login and logout in the sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
 
+  def session_params
+    params.require(:account).permit(:username, :password)
+  end
+
+  def create
+    account = Account.find_by(username: session_params[:username]).try(:authenticate, session_params[:password])
+    if account
+      session[:session_token] = account.session_token
+      redirect_to rooms_path
+    else
+      flash[:notice] = 'Incorrect username or password'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:session_token] = nil
+    flash[:notice] = 'Successfully logged out.'
+    redirect_to login_path
+  end
+
 end
 
