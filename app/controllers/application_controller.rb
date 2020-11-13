f081f0bec1c95feb1f87852fb1919b41076a9907
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-13 09:31:41 -0600

    Added the instance variable current_user to the ApplicationController so the application always redirects to login page for actions a user must be logged in for.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  @current_user
+
+  def set_current_user
+    @current_user ||= session[:session_token] && Account.find_by_session_token(session[:session_token])
+    flash[:notice] = @current_user.nil? ? 'Please login before using our application' : "Welcome #{@current_user.name}!"
+    redirect_to '/login' and return unless @current_user
+  end
+
 end
