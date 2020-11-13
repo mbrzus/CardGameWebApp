cc637f3da026e9643ebc1dde7970e0321549e62c
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-12 19:40:05 -0600

    Upon room creation, a room_token is created that is stored in session. When a player tries to join a room, they must enter the room_token instead of the id. This change is intended to improve the security of the room, so people can't just guess numbers to join rooms.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,14 @@
 class RoomsController < ApplicationController
 
   def show
-    @room_id = params[:id]
-
     # get the player_id stored in this sessions id
-    @player = session[@room_id.to_s]
+    @room_id = Room.find_by_room_token(session[:room_token]).id
+    @deck = Card.where(owned_by: 'dealer', room_id: @room_id)
+    @player = session[@room_id]
     # if the player_id exists, join the game
     if !! @player
       # get all the game info
     else
-      session[:room_to_join] = @room_id
       # the player has not logged in or doesnt exist, redirect to where they can create
       redirect_to new_player_path
     end
@@ -27,15 +26,16 @@ class RoomsController < ApplicationController
     # room has no info so just create an empty object
     new_room = Room.create_room!
     @room_id = new_room.id
-    session[:room_token] = @room_id
-    redirect_to room_path(:id => @room_id)
+    session[:room_token] = new_room.room_token
+    puts session[:room_token]
+    redirect_to room_path(id: session[:room_token])
   end
 
   # the main page links to here. This controller just gets the room_id
   # from the submitted form and redirects the user to that path
   def join_room
-    session[:room_to_join] = params.require(:room_id)["room_id"]
-    redirect_to room_path(:id => session[:room_to_join])
+    session[:room_token] = params.require(:room)["room_token"]
+    redirect_to room_path(id: session[:room_token])
   end
 
   def edit
@@ -43,7 +43,10 @@ class RoomsController < ApplicationController
   end
 
   def update
-
+    @room_id = Room.find_by_room_token(session[:room_token]).id
+    @deck = Card.where(owned_by: 'dealer', room_id: @room_id)
+    card = @deck.pop
+    card.update_attributes!(owned_by: session[@room_id])
   end
 
   def destroy
