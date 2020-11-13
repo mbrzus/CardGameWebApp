06256ab5b9e65251702c8684362a3d7304184890
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-13 09:28:10 -0600

    Added a before_filter to set the current user except for the controller actions new and create and also implemented the same controller actions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,3 +1,23 @@
 class AccountsController < ApplicationController
 
+  before_filter :set_current_user, except: %i[new create]
+
+  def account_params
+    params.require(:account).permit(:name, :username, :email, :password)
+  end
+
+  def new
+    redirect_to rooms_path if @current_user
+  end
+
+  def create
+    account_params[:email].downcase!
+    account = Account.new(account_params)
+    flash[:notice] = account.valid? ? 'Account created! Please login to continue' : account.error_message
+    redirect_to signup_path and return unless account.valid?
+
+    Account.create_account!(account_params)
+    redirect_to login_path
+  end
+
 end
