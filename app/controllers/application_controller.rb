ec7e75df964914754f2fe5ff95b944ee3be18de7
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-12 19:32:50 -0600

    removed the instance variable current_room

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  @current_room
-
-  def set_current_room
-    @current_room ||= session[:room_token] && Room.find_by_room_token(session[:room_token])
-    redirect_to '/rooms' and return unless @current_room
-  end
 end
