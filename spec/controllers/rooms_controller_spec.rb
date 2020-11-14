4cb50143dbaffc0add88add224188e40f4908e60
mbrzus <michal-brzus@uiowa.edu>
2020-11-13 19:37:59 -0600

    fix 6 out 10 failing tests. Tests fauilure due to changes in implementation

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -53,7 +53,7 @@ describe RoomsController do
     it 'should redirect the user to a page where they can create a new user (if no session is set)' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :show, { :id => token }, { :room_to_join => "1" }
       expect(response).to redirect_to('/players/new')
     end
@@ -64,7 +64,7 @@ describe RoomsController do
     end
     it '(the join_room action) should direct the user to the show action' do
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :join_room, { :room_id => { "room_id" => "1"} }, { }
       expect(response).to redirect_to("/rooms/#{token}")
     end
@@ -74,14 +74,14 @@ describe RoomsController do
     it 'should redirect the user home page' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :destroy, { :id => token }, { :room_to_join => 1 }
       expect(response).to redirect_to('/rooms')
     end
     it 'should delete room from database Room table' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :destroy, { :id => token }, { :room_to_join => "1" }
       room = Room.where(id: 1)
       expect(room.length).to be(0)
@@ -89,7 +89,7 @@ describe RoomsController do
     it 'should delete cards associated with the room' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :destroy, { :id => token }, { :room_to_join => "1" }
       cards = Card.where(room: room)
       expect(cards.length).to be(0)
@@ -97,7 +97,7 @@ describe RoomsController do
     it 'should delete players associated with the room' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :destroy, { :id => token }, { :room_to_join => "1" }
       players = Player.where(room: room)
       expect(players.length).to be(0)
@@ -108,7 +108,7 @@ describe RoomsController do
     it 'should redirect the user to the same page' do
       # there is no information for Rooms besides auto-generated id
       room = Room.find(1)
-      token = room.token
+      token = room.room_token
       post :reset, { :id => token }, { :room_to_join => "1" }
       expect(response).to redirect_to("/rooms/#{token}")
     end
@@ -117,7 +117,7 @@ describe RoomsController do
       room = Room.find(1)
       player = Player.where(room: room, name: "Ted")[0]
       Card.create!({:room => room, :value => 'A', :suit => 'spades', :player => player, :image_url => 'AS.png'})
-      post :reset, { :id => room.token }, { :room_to_join => "1" }
+      post :reset, { :id => room.room_token }, { :room_to_join => "1" }
       cards = Card.where(room: room)
       dealer = Player.where(room: room, name: "dealer")[0]
       helper = TRUE
