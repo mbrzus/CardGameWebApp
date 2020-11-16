850f9c3e09fee98be58f3c318687bc93465c8023
ShriramVarada <shriram.varadarajan@gmail.com>
2020-11-16 09:50:04 -0600

    Modified hash values to strings

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -242,7 +242,7 @@ class CardsController < ApplicationController
   def give_cards
     room_id = params[:room_id].to_i
     giving_player = session[room_id.to_s]
-    cards_to_give = Card.where(room_id: giving_player[:room_id], player_id: giving_player[:id])
+    cards_to_give = Card.where(room_id: giving_player["room_id"], player_id: giving_player["id"])
     @cards_to_give_array = cards_to_give.to_a
 
     @players = Player.where(room_id: room_id)
