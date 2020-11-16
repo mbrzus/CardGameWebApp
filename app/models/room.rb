ab87d3072aa1a2f2bbd6f2e819c3e9f54ee790e3
Daniel Illg <dillg@uiowa.edu>
2020-11-16 10:40:39 -0600

    added function get_public_room_information

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,4 +10,26 @@ class Room < ActiveRecord::Base
     new_room.save!
     return new_room.id
   end
+
+  # gets information about the public rooms and returns them to the caller
+  def self.get_public_rooms_information
+    # will be a list of hashes that contain information about public rooms
+    # The information needed is room name, room id, and a list of player names within that room
+    public_rooms_information = []
+    # get the public rooms out of the database
+    public_rooms = Room.where(:public => true)
+    # go through the public rooms and make the hash object
+    public_rooms.each do |room|
+      player_names_list = []
+      # get all the players in the room and add their name to the list
+      Player.where(:room => room).each do |player|
+        player_names_list << player.name
+      end
+      # now that we have all the information, create the hash and add it in
+      public_rooms_information << { :room_name => room.name, :room_id => room.id, :player_names_list => player_names_list }
+    end
+
+    # return the public rooms information to the user
+    return public_rooms_information
+  end
 end
