b7723987978ad0a5cd027c07f082080f30787789
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-20 14:31:41 -0600

    uncommented out a line that compresses the CSS in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # JavaScripts were causing errors when deploying to Heroku. Found a solution at:
   # https://github.com/rails/webpacker/issues/1285
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
