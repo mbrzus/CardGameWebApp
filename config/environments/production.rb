84f2518534e3cbd51903c8db25f7abeaaa9d1a3a
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-20 15:35:58 -0600

    changed the line in production.rb to fix compiler issues when deploying to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  # # JavaScripts were causing errors when deploying to Heroku. Found a solution at:
+  #   # https://github.com/rails/webpacker/issues/1285
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  # JavaScripts were causing errors when deploying to Heroku. Found a solution at:
-  # https://github.com/rails/webpacker/issues/1285
-  #config.assets.js_compressor = Uglifier.new(harmony: true)
-  #config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
