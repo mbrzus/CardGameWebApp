d9e57d2bcae910d2712e728f7fd7122a8ca20ab4
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-20 14:23:30 -0600

    Added a line to production.rb that will hopefully resolve the compiling errors when deploying to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  # JavaScripts were causing errors when deploying to Heroku. Found a solution at:
+  # https://github.com/rails/webpacker/issues/1285
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
