d6909b04a868c1d85d11d2ad12a7fff5fcc4d76c
Jacob Watters <jacob-watters@uiowa.edu>
2020-11-20 15:24:05 -0600

    changed the configuration of Uglifier again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # JavaScripts were causing errors when deploying to Heroku. Found a solution at:
   # https://github.com/rails/webpacker/issues/1285
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
