0eeca8aac5df8a0e3e98d4ccb7292a3dbcb398db
Daniel Illg <dillg@uiowa.edu>
2020-11-28 14:16:48 -0600

    added rspec tests to test a function that generates 52 card objects to be displayed to the user

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,5 +4,14 @@ require 'rails_helper'
 describe Card do
 
   # Add RSpec tests here
-
+  describe "Get deck of cards", type: :view do
+    it 'it should return a list of 52 items' do
+      list_of_cards = Card.get_deck_of_cards
+      expect(list_of_cards.length).to eq(52)
+    end
+    it 'The items returned should be card objects' do
+      list_of_cards = Card.get_deck_of_cards
+      expect(list_of_cards[0].class).to be(Card)
+    end
+  end
 end
