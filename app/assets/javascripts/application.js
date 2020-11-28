e7c628872214c58e73b4e58cb040d738eade28dc
Daniel Illg <dillg@uiowa.edu>
2020-11-28 14:22:04 -0600

    added in a javascript function that changes the border color of cards when selection/deselecting them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,27 @@ function join_room(room_id) {
     document.getElementById('room_id_input').value = room_id;
     document.getElementById('join_room_button').click();
 }
+
+
+// this function takes as an argument a card html object form the create room screen
+// If this card is currently selected, we should unselect it and change its border color to show the user
+// If it is currently unselected, we should do the opposite
+function toggle_selection(card_html_object) {
+    // first, get whether the card is selected
+    var current_selected_value = card_html_object.getAttribute("data-selected");
+
+    // the card is currently selected to be in the room
+    if (current_selected_value == "true") {
+        // set the selected value
+        card_html_object.setAttribute("data-selected", "false");
+        // set the border color to transparent
+        card_html_object.style.borderColor = "rgb(0,0,0,0)";
+    }
+    // the card is currently not selected
+    else {
+        // set its selected value to true
+        card_html_object.setAttribute("data-selected", "true");
+        // set the border color to yellow
+        card_html_object.style.borderColor = "yellow";
+    }
+}
