65b46ef1ab95806981e0ff5cc438ee8e6b99330e
Daniel Illg <dillg@uiowa.edu>
2020-11-28 14:48:34 -0600

    add a javascript function that gets the values and suits of all selected cards, attatches this calue to the form, and submits the form to the backend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,33 @@ function toggle_selection(card_html_object) {
         card_html_object.style.borderColor = "yellow";
     }
 }
+
+// this function will submit the create room form
+// before it can do that, however, it must get the list of cards the player has selected
+// to be used and turn it into the specified data format
+// specifically -> value-suit,value-suit,value-suit,...
+function submit_create_room_form() {
+    // format value-suit,value-suit,value-suit,...
+    var selected_cards = "";
+    // first, get a list of all the card html objects
+    var card_html_objects = document.getElementsByClassName("playing-cards-box")[0].children;
+    // now, go through the children and check their data-selection attribute
+    // length - 1 is because there is an extra padding element at the end
+    for (var child_index = 0; child_index < card_html_objects.length - 1; child_index++) {
+        var card_html_object = card_html_objects[child_index];
+
+        if (card_html_object.getAttribute("data-selected") == "true") {
+            // the id is already in the form we need
+            var value_suit = card_html_object.id;
+            // add the value-suit string to the total selected cards
+            selected_cards += value_suit + ",";
+        }
+    }
+
+    // remove the trailing comma
+    selected_cards.replace(/,$/, "");
+    // set the hidden input in the form to the selected cards
+    document.getElementById("cards_to_use").value = selected_cards;
+    // submit the form
+    document.getElementById("create_room_form").submit();
+}
