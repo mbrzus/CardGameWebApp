450a8695f90f0cb3a009bd1bffd97677f87988a3
Daniel Illg <dillg@uiowa.edu>
2020-12-05 13:20:16 -0600

    fixed an error in my controller specs

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -30,19 +30,16 @@ describe PlayersController do
       fake_results = { :name => "Daniel",
                        :room => room }
       expect(Player).to receive(:login).with(fake_results)
-      post :create, { :player_name => { "name" => "Daniel" } }
+      post :create, { :player_name => { "name" => "Daniel" } }, { :room_token => Room.find(1).room_token }
     end
     it 'should call redirect to the game page' do
       post :create, { :player_name => { "name" => "Daniel" } }
       expect(response).to redirect_to(room_path(session[:room_token]))
     end
-    it 'if the max occupancy has been reached and the player login doesnt exist, it should redirect to rooms' do
+    it 'if the max occupancy has been reached and the player login doesnt exist, it should redirect to rooms', :room_3 do
       # there are too many players within the room already
-      room = Room.find(3)
-      fake_results = { :name => "Daniel",
-                       :room => room }
+      post :create, { :player_name => { "name" => "Daniel" } }, { :room_token => Room.find(3).room_token }
       expect(response).to redirect_to('/rooms')
-      post :create, { :player_name => { "name" => "Daniel" } }
     end
 
 
